407c079a5c5b5ba8bb193ef31bd8504213c2e7e6
danilobatson <djbatson19@gmail.com>
2022-01-23 06:28:10 -0800

    feat: Restaurant info set up with dummy data

--- src/components/restaurant-info.component.js
+++ src/components/restaurant-info.component.js
@@ -3,10 +3,21 @@ import { SafeAreaView, StyleSheet, Text, View, StatusBar } from 'react-native';
 import { Searchbar } from "react-native-paper";
 
 
-export const RestaurantInfo = () => {
+export const RestaurantInfo = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
   return (
     <>
-    <Text>Restaurant Info</Text>
+      <Text>{name}</Text>
     </>
   )
 }
