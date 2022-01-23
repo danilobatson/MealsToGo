646ea367541b3c04b753ed15687bc2824cc45edf
danilobatson <djbatson19@gmail.com>
2022-01-23 01:15:00 -0800

    Test

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Working on Meals To Go App</Text>
+      <Text>Working on Meals To Go Application</Text>
       <StatusBar style="auto" />
     </View>
   );
