638158b179f7ef79144f4f2962bd16b67036e4fc
danilobatson <djbatson19@gmail.com>
2022-01-24 01:35:45 -0800

    feat: Add stars for rating

--- src/components/restaurant-info-card.component.js
+++ src/components/restaurant-info-card.component.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Button, Card } from 'react-native-paper';
-import {Text, View} from 'react-native'
+import { Text, View } from 'react-native'
 import styled from 'styled-components'
+import { SvgXml } from 'react-native-svg'
+import star from '../../assets/star'
 
 const RestaurantCard = styled(Card)`
   backgroundColor: white;
@@ -17,15 +19,18 @@ const Address = styled(Text)`
   color: ${props => props.theme.colors.ui.primary};
   fontSize: ${props => props.theme.fontSizes.title};
 `
-const Rating = styled(Text)`
-  font-family: ${(props) => props.theme.fonts.monospace};
-  color: ${props => props.theme.colors.ui.secondary};
-  fontSize: ${props => props.theme.fontSizes.body};
-`
+
 const CardCover = styled(Card.Cover)`
   padding: ${props => props.theme.space.L};
   background-color: white;
 `;
+
+const Rating = styled(View)`
+flexDirection: row;
+  paddingTop: ${props => props.theme.space.MD};
+  paddingBottom: ${props => props.theme.space.MD};
+
+`
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
@@ -39,6 +44,9 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     rating = 4,
     isClosedTemporarily,
   } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)))
+
   return (
     <>
       <RestaurantCard elevation={5} >
@@ -46,7 +54,13 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
         <CardCover key={name} source={{ uri: photos[0] }} />
         <Card.Content>
           <Address >{address} </Address>
-          <Rating >Rating: {rating}</Rating>
+          <Rating>
+            {ratingArray.map(() => {
+              return (
+                <SvgXml xml={star} width={20} height={20} />
+              )
+            })}
+          </Rating>
         </Card.Content>
         <Card.Actions>
           <Button>Cancel</Button>
